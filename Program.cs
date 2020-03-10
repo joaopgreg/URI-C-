97e5c6a874ab6508b84c17cd6fc7707f74518e4d
joaopgreg <47890086+joaopgreg@users.noreply.github.com>
2020-03-10 16:25:15 -0300

    Uri 1165
    Exercício uri 1165 em c#

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace uri1165
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                int teste = int.Parse(Console.ReadLine());
+                int Div = 0;
+
+                for (int j = 1; j <= teste; j++)
+                {
+                    if (teste % j == 0)
+                    {
+                        Div += 1;
+                    }
+                }
+
+                if (Div == 2)
+                {
+                    Console.WriteLine(teste + " eh primo");
+                }
+
+                else
+                {
+                    Console.WriteLine(teste + " nao eh primo");
+                }
+            }
+        }
+    }
+}
